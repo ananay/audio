d779b7b9d78610172aeecc6c680fe677439c5208
Jere Vaara <jerevaara@gmail.com>
2016-10-30 16:08:15 +0200

    google speech api

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express'
 import multer from 'multer'
+import bodyParser from 'body-parser'
+// import fetch from 'node-fetch'
+import speech from 'google-speech-api'
+
+const opts = {
+  key: process.env.SPEECH_API_KEY
+}
 
 const app = express()
 const upload = multer()
 
 app
-.get('/', (req, res) => res.send('up'))
-.post('/', upload.single('audio'), (req, res) => {
+.use(express.static('./public'))
+.use(bodyParser.json())
+.post('/file', upload.single('audio'), (req, res) => {
   console.log('Received files: ' + req.file)
   res.send('OK')
 })
-
+.post('/', (req, res) => {
+  request
+  .get(req.body.audioFileUrl)
+  .pipe(speech(opts, (err, res) => {
+    console.log(err)
+    console.log(res)
+    res.send('OOK')
+  }))
+})
 .listen(process.env.PORT || 5000)
